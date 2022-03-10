fa67ccb9637b09fdc7f7d8f7a30257deccbdb705
ResulCan0 <resulcan.corduk@pru.edu.tr>
2022-03-10 23:43:43 +0300

    Eklendi

---
+++ ERP.Business/Extentions/ExceptionMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace ERP.Business;
+
+public class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+
+            await context.Response.WriteAsJsonAsync(ex.Data);
+        }
+    }
+}
---
+++ ERP.Business/Helper/UseFriendlyException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace ERP.Business.Helper;
+
+public class UseFriendlyException: CustomException
+{
+    public Enum ExceptionTypeEnum { get; set; }
+    public UseFriendlyException(Enum exceptionTypeEnum, List<string>? errors = default, HttpStatusCode httpStatusCode = HttpStatusCode.NoContent)
+        : base("Failures Occured.", errors, httpStatusCode)
+    {
+        ExceptionTypeEnum = exceptionTypeEnum;
+    }
+}
